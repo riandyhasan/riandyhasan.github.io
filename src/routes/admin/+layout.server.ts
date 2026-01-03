6c7a9718489b23c032874edd70362b5f1ec0e3f2
Yandy <riandyhsn@gmail.com>
2026-01-03 17:56:40 +0700

    fix: exclude admin routes from prerendering

--- src/routes/admin/+layout.server.ts
+++ src/routes/admin/+layout.server.ts
@@ -1,5 +1,7 @@
 import { error } from '@sveltejs/kit';
 
+export const prerender = false;
+
 export async function load() {
 	if (process.env.NODE_ENV !== 'development') {
 		error(404, 'Not found');
